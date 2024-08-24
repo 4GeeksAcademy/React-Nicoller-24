b71be90070179f4d6c665cc2360f285022e8984b
Nicoller-24 <166450090+Nicoller-24@users.noreply.github.com>
2024-08-24 15:50:19 +0000

    Proyecto mejorado

--- src/js/component/containercards.jsx
+++ src/js/component/containercards.jsx
@@ -2,22 +2,41 @@ import React from "react";
 import Cards from "./card";
 
 const ContainerCards = () => {
-    return <>
-       <div className="d-flex" style={{marginLeft: "4%", marginBottom: "5%"}}>
-            <div style={{marginRight: "2%"}}>
-                <Cards/>
+    return (
+        <>
+            <div
+                className="d-flex flex-wrap justify-content-center"
+                style={{ marginLeft: "4%", marginBottom: "5%" }}
+            >
+                <div style={{ margin: "1%" }}>
+                    <Cards />
+                </div>
+                <div style={{ margin: "1%" }}>
+                    <Cards />
+                </div>
+                <div style={{ margin: "1%" }}>
+                    <Cards />
+                </div>
+                <div style={{ margin: "1%" }}>
+                    <Cards />
+                </div>
             </div>
-            <div style={{marginRight: "2%"}}>
-                <Cards/>
-            </div>
-            <div style={{marginRight: "2%"}}>
-                <Cards/>
-            </div>
-            <div style={{marginRight: "2%"}}>
-                <Cards/>
-            </div>
-         </div>
-    </>
-}
 
-export default ContainerCards
+            <style jsx>{`
+                @media (max-width: 768px) {
+                    .d-flex > div {
+                        flex: 0 0 45%;
+                    }
+                }
+
+                @media (max-width: 480px) {
+                    .d-flex > div {
+                        flex: 0 0 90%;
+                    }
+                }
+            `}</style>
+        </>
+    );
+};
+
+export default ContainerCards;
--- src/js/component/navbar.jsx
+++ src/js/component/navbar.jsx
@@ -1,25 +1,32 @@
 import React from "react";
 
 const NavBar = () => {
-   return <>
-    <nav className="navbar bg-dark navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
-    <div className="container-fluid">
-    <a className="navbar-brand text-white" href="#">Bootstrap Start</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
-      <div className="navbar-nav ">
-        <a className="nav-link active text-white" aria-current="page" href="#">Home</a>
-        <a className="nav-link text-secondary" href="#">About</a>
-        <a className="nav-link text-secondary" href="#">Service</a>
-        <a className="nav-link text-secondary" href="#">Contact</a>
-      </div>
-    </div>
-  </div>
-   
-    </nav>
-   </>
+   return (
+    <>
+      <nav className="navbar bg-dark navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
+        <div className="container-fluid">
+          <a className="navbar-brand text-white" href="#">Bootstrap Start</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+            <div className="navbar-nav">
+              <a className="nav-link active text-white" aria-current="page" href="#">Home</a>
+              <a className="nav-link text-secondary" href="#">About</a>
+              <a className="nav-link text-secondary" href="#">Service</a>
+              <a className="nav-link text-secondary" href="#">Contact</a>
+            </div>
+          </div>
+        </div>
+      </nav>
+
+      <style jsx>{`
+        .navbar-toggler-icon {
+          background-image: url("data:image/svg+xml;charset=UTF8,%3Csvg viewBox='0 0 16 16' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath stroke='rgba%28255, 255, 255, 1%29' stroke-width='1.5' d='M1 2.5h14M1 8h14M1 13.5h14'/%3E%3C/svg%3E");
+        }
+      `}</style>
+    </>
+   );
 }
 
-export default NavBar
+export default NavBar;
